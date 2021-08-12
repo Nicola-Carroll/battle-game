668987a0923158d8f3c2d8ab244d69a70d58091f
Ahsan <rasul.ahsan@gmail.com>
2021-08-12 16:42:19 +0100

    added game class and working on test

---
+++ lib/game.rb
@@ -0,0 +1,15 @@
+class Game
+  def initialize(name1, name2, player_class:)
+    @player_class = player_class
+    @player1 = player_class.new(name1)
+    @player2 = player_class.new(name2)
+  end
+
+  def player2
+    @player2
+  end
+
+  def attack(amount)
+    @player2.reduce_hp(amount) 
+  end
+end
