d4ea31df3aa0fc7d72c74d3fec5b81353425d651
Ahsan <rasul.ahsan@gmail.com>
2021-08-12 15:26:48 +0100

    used global variable and created a Player class

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 require 'sinatra'
 require "sinatra/reloader" if development?
-
+require_relative 'lib/player'
 
 class Battle < Sinatra::Base
 
@@ -16,14 +16,14 @@ class Battle < Sinatra::Base
 
   post '/names' do
     p params
-    session[:Player1] = params[:Player1]
-    session[:Player2] = params[:Player2]
+    $player_1 = Player.new(params[:Player1]).name
+    $player_2 = Player.new(params[:Player2]).name
     redirect '/play'
   end
 
   get '/play' do
-    @player_1 = session[:Player1] 
-    @player_2 = session[:Player2]
+    @player_1 = $player_1
+    @player_2 = $player_2
     erb(:play)
   end
 
@@ -32,8 +32,8 @@ class Battle < Sinatra::Base
   # end
 
   get '/attack' do
-    @player_1 = session[:Player1] 
-    @player_2 = session[:Player2]
+    @player_1 = $player_1
+    @player_2 = $player_2
     erb(:attack)
   end
 
